1eebebd070467ea11c18c6ad7efe67e3d0f5215e
yuta.kato <yyyutaa7@gmail.com>
2021-09-27 13:43:37 +0000

    ruby-oop1

--- team.rb
+++ team.rb
@@ -1,29 +1,28 @@
 class Team
-  attr_accessor :name, :win, :lose, :draw, :suffix
+  attr_accessor :name, :win, :lose, :draw
   
   def initialize(name, win, lose, draw)
     self.name = name
     self.win = win
     self.lose = lose
     self.draw = draw
-    self.suffix = suffix
   end
   
-  def calc_win_rate(ritu)
+  def calc_win_rate(show_team_result)
     puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は0.#{win.to_f/(win+lose)}です。"
   end
 end
 
-Giants = Team.new("Giants", 67, 45, 8)
-Tigers = Team.new("Tigers", 60, 53, 7)
-Dragons = Team.new("Dragons", 60, 55, 5)
-BayStars = Team.new("BayStars", 56, 58, 6)
-Carp = Team.new("Carp", 52, 56, 12)
-Swallows = Team.new("Swallows", 41, 69, 10)
+giants = Team.new("Giants", 67, 45, 8)
+tigers = Team.new("Tigers", 60, 53, 7)
+dragons = Team.new("Dragons", 60, 55, 5)
+bayStars = Team.new("BayStars", 56, 58, 6)
+carp = Team.new("Carp", 52, 56, 12)
+swallows = Team.new("Swallows", 41, 69, 10)
 
-Giants.calc_win_rate("勝率")
-Tigers.calc_win_rate("勝率")
-Dragons.calc_win_rate("勝率")
-BayStars.calc_win_rate("勝率")
-Carp.calc_win_rate("勝率")
-Swallows.calc_win_rate("勝率")
+giants.calc_win_rate("勝率")
+tigers.calc_win_rate("勝率")
+dragons.calc_win_rate("勝率")
+bayStars.calc_win_rate("勝率")
+carp.calc_win_rate("勝率")
+swallows.calc_win_rate("勝率")
